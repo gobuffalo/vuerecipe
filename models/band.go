f0bdff5072fe5ab3f3704ab3c355961b513ae51f
Tom <20537914+t-0-m-1-3@users.noreply.github.com>
2018-03-31 19:42:42 -0400

    switched route path to gobuffalo (#3)

--- models/band.go
+++ models/band.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/markbates/pop"
-	"github.com/markbates/validate"
-	"github.com/markbates/validate/validators"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
 	"github.com/satori/go.uuid"
 )
 
