38dc73b404bd63db7dc5eb234caf3c019287d310
Stanislas Michalak <stanislas.michalak@gmail.com>
2019-02-23 15:31:06 +0100

    Upgrade webpack config and js deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,123 +1,100 @@
-var webpack = require("webpack");
+const Webpack = require("webpack");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var ManifestPlugin = require("webpack-manifest-plugin");
-var PROD = process.env.NODE_ENV || "development";
+const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-module.exports = {
-  entry: {
-    application: [
-      "./assets/js/application.js",
-      "./node_modules/jquery-ujs/src/rails.js",
-      "./assets/css/application.scss"
-    ]
+const configurator = {
+  entries: function(){
+    var entries = {
+      application: [
+        "./assets/js/application.js",
+        "./node_modules/jquery-ujs/src/rails.js",
+        "./assets/css/application.scss"
+      ],
+    }
+    return entries
   },
-  resolve: {
-    alias: {
-      vue$: `${__dirname}/node_modules/vue/dist/vue.esm.js`,
-      router$: `${__dirname}/node_modules/vue-router/dist/vue-router.esm.js`
+  plugins() {
+    var plugins = [
+      new CleanObsoleteChunks(),
+      new Webpack.ProvidePlugin({$: "jquery",jQuery: "jquery"}),
+      new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
+      new CopyWebpackPlugin([{from: "./assets",to: ""}], {copyUnmodified: true,ignore: ["css/**", "js/**", "src/**"] }),
+      new Webpack.LoaderOptionsPlugin({minimize: true,debug: false}),
+      new ManifestPlugin({fileName: "manifest.json"}),
+      new VueLoaderPlugin()
+    ];
+
+    return plugins
+  },
+
+  moduleOptions: function() {
+    return {
+      rules: [
+        {test: /\.html$/, loader: "raw-loader"},
+        {test: /\.vue/, loader: "vue-loader"},
+        {
+          test: /\.s[ac]ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: {sourceMap: true}},
+            { loader: "sass-loader", options: {sourceMap: true}}
+          ]
+        },
+        { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/},
+        { test: /\.jsx?$/,loader: "babel-loader",exclude: /node_modules/ },
+        { test: /\.(woff|woff2|ttf|svg)(\?v=\d+\.\d+\.\d+)?$/,use: "url-loader"},
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,use: "file-loader" },
+        { test: require.resolve("jquery"),use: "expose-loader?jQuery!expose-loader?$"},
+        { test: /\.go$/, use: "gopherjs-loader"}
+      ]
     }
   },
-  output: {
-    filename: "[name].[hash].js",
-    path: `${__dirname}/public/assets`
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-    new ExtractTextPlugin("[name].[hash].css"),
-    new CopyWebpackPlugin(
-      [
-        {
-          from: "./assets",
-          to: ""
-        }
-      ],
-      {
-        ignore: ["css/**", "js/**"]
-      }
-    ),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new ManifestPlugin({
-      fileName: "manifest.json"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        loader: "raw-loader"
-      },
-      {
-        test: /\.vue/,
-        loader: "vue-loader"
-      },
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            {
-              loader: "css-loader",
-              options: {sourceMap: true}
-            },
-            {
-              loader: "sass-loader",
-              options: {sourceMap: true}
-            }
-          ]
-        })
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=application/octet-stream"
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: "file-loader"
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: "url-loader?limit=10000&mimetype=image/svg+xml"
-      },
-      {
-        test: require.resolve("jquery"),
-        use: "expose-loader?jQuery!expose-loader?$"
-      }
-    ]
-  }
-};
+  buildConfig: function(){
+    // NOTE: If you are having issues with this not being set "properly", make
+    // sure your GO_ENV is set properly as `buffalo build` overrides NODE_ENV
+    // with whatever GO_ENV is set to or "development".
+    const env = process.env.NODE_ENV || "development";
 
-if (PROD != "development") {
-  module.exports.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
+    var config = {
+      mode: env,
+      entry: configurator.entries(),
+      output: {filename: "[name].[hash].js", path: `${__dirname}/public/assets`},
+      plugins: configurator.plugins(),
+      module: configurator.moduleOptions(),
+      resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+          vue$: `${__dirname}/node_modules/vue/dist/vue.esm.js`,
+          router$: `${__dirname}/node_modules/vue-router/dist/vue-router.esm.js`
+        }
       },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
+    }
+
+    if( env === "development" ){
+      config.plugins.push(new LiveReloadPlugin({appendScriptTag: true}))
+      return config
+    }
+
+    const uglifier = new UglifyJsPlugin({
+      uglifyOptions: {
+        beautify: false,
+        mangle: {keep_fnames: true},
+        output: {comments: false},
+        compress: {}
+      }
     })
-  );
+
+    config.optimization = {
+      minimizer: [uglifier]
+    }
+
+    return config
+  }
 }
+
+module.exports = configurator.buildConfig()
